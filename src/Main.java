00c4e9c10fea6fa61cc962682eaa86e1b4c10f8c
atilimkoca1 <atilimkoca@gmail.com>
2022-09-26 17:34:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int row, col;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen satır sayısını giriniz : " );
+        row = input.nextInt();
+        System.out.print("Lütfen sütun sayısını giriniz : ");
+        col = input.nextInt();
+
+
+        MineSweeper mine = new MineSweeper(row, col);
+        mine.run();
+
+    }
+}
